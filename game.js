10fe29637e38ecd9a74e89316e1af52d3f6aa09b
Ayman <ayman.farhat1@gmail.com>
2014-08-15 22:58:13 +0300

    Setting up the stage

---
+++ game.js
@@ -0,0 +1,37 @@
+$ = {};
+
+$.width = 700;
+$.height = 200;
+
+$.init = function () {
+    $.RATIO = $.width / $.height;
+    $.currentWidth = $.width;
+    $.currentHeight = $.height;
+    $.canvas = document.getElementsByTagName('canvas')[0];
+    $.canvas.width = $.width;
+    $.canvas.height = $.height;
+    $.ctx = $.canvas.getContext('2d');
+
+    $.resize();
+};
+
+
+$.resize = function() {
+
+    $.currentHeight = window.innerHeight;
+    $.currentWidth = $.currentHeight * $.ratio;
+
+    if ($.android || $.ios) {
+        document.body.style.height = (window.innerHeight + 50) + 'px';
+    }
+
+    $.canvas.style.width = $.currentWidth + 'px';
+    $.canvas.style.height = $.currentHeight + 'px';
+
+    window.setTimeout(function() {
+            window.scrollTo(0,1);
+    }, 1);
+}
+
+window.addEventListener('load', $.init, false);
+window.addEventListener('resize', $.resize, false);
