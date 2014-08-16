f447e10b17095c6ae11fb2c294a57f532a00e78d
Ayman <ayman.farhat1@gmail.com>
2014-08-16 09:29:10 +0300

    Render a simple hero object on screen

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 $ = {};
 
-$.width = 700;
-$.height = 200;
+$.width = 800;
+$.height = 600;
 
 $.init = function () {
     $.RATIO = $.width / $.height;
@@ -13,6 +13,9 @@ $.init = function () {
     $.ctx = $.canvas.getContext('2d');
 
     $.resize();
+
+    var myHero = new $.Hero();
+    myHero.render();
 };
 
 
@@ -32,6 +35,34 @@ $.resize = function() {
             window.scrollTo(0,1);
     }, 1);
 }
+
+$.Draw = {
+    clear: function() {
+        $.ctx.clearRect(0, 0, $.width, $.height);
+    },
+
+    rect: function(x, y, w, h, col) {
+        $.ctx.fillStyle = col;
+        $.ctx.fillRect(x, y, w, h);
+    },
+
+    text: function(string, x, y, size, col) {
+        $.ctx.font = 'bold '+size+'px Monospace';
+        $.ctx.fillStyle = col;
+        $.ctx.fillText(string, x, y);
+    }
+};
+
+$.Hero = function () {
+	this.x = 5;
+	this.y = $.canvas.height -41;
+	this.background = 'red';
+};
+
+$.Hero.prototype.render = function () {
+    $.Draw.clear();
+    $.Draw.rect(this.x, this.y, 40, 40, 'red');
+};
 
 window.addEventListener('load', $.init, false);
 window.addEventListener('resize', $.resize, false);
