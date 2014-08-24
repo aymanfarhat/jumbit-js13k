b2a0168ad222d6f2b60541ade14b8425886439a8
Ayman <ayman.farhat1@gmail.com>
2014-08-24 23:05:42 +0300

    Minify css files and include in html build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,56 @@
 module.exports = function(grunt) {
- 
     grunt.initConfig({
-    clean: {
-        build: {
-            src: ['build/*', '!build/.gitignore']
-        }
-    },
-    uglify: {
-      build: {
-        options: {
-          mangle: true,
-          compress:true
+        clean: {
+            build: {
+                src: ['build/*', '!build/.gitignore']
+            }
         },
-        files: {
-          'build/game.min.js': ['build/game.concat.js']
-        }
-      }
-    },
-    processhtml: {
-        build: {
-         options: {
-           customBlockTypes: ['include-js-file.js'],
-           process: true
-         },
-         files: {
-           'build/index.min.html': ['src/index.html']
-         }
-        }
-    },
-    htmlmin: {
-        build: {
-         options: {
-           removeComments: true,
-           collapseWhitespace: true
-         },
-         files: {
-           'build/index.min.html': 'build/index.min.html'
-         }
-        }
-    },
-    concat: {
-        build: {
-            src: ['src/js/game.js', 'src/**/*.js'],
-            dest: 'build/game.concat.js',
+        uglify: {
+          build: {
+            options: {
+              mangle: true,
+              compress:true
+            },
+            files: {
+              'build/game.min.js': ['build/game.concat.js']
+            }
+          }
         },
-    }
+        processhtml: {
+            build: {
+             options: {
+               customBlockTypes: ['include-script-file.js'],
+               process: true
+             },
+             files: {
+               'build/index.min.html': ['src/index.html']
+             }
+            }
+        },
+        htmlmin: {
+            build: {
+             options: {
+               removeComments: true,
+               collapseWhitespace: true
+             },
+             files: {
+               'build/index.min.html': 'build/index.min.html'
+             }
+            }
+        },
+        concat: {
+            build: {
+                src: ['src/js/game.js', 'src/**/*.js'],
+                dest: 'build/game.concat.js',
+            },
+        },
+        cssmin: {
+            minify: {
+                files: {
+                    'build/style.min.css': ['src/css/**/*.css']
+                }
+            }
+        }
     });
  
     grunt.loadNpmTasks('grunt-contrib-clean');
@@ -52,10 +58,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-processhtml');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     grunt.registerTask(
         'build',
         'Compiles all assets to the build directory',
-        ['clean', 'concat', 'uglify', 'processhtml', 'htmlmin']
+        ['clean', 'concat', 'uglify', 'cssmin', 'processhtml', 'htmlmin']
     );
 };
