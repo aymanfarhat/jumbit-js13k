5da600cd7b13f935315ffcff88cac489b9a467ae
Ayman <ayman.farhat1@gmail.com>
2014-09-12 17:15:42 +0300

    Render formation and obstacles from preloaded images for faster rendering

--- src/js/draw.js
+++ src/js/draw.js
@@ -1,4 +1,5 @@
 $.Draw = {
+    obstacleObj: null,
     clear: function() {
         $.ctx.clearRect(0, 0, $.width, $.height);
     },
@@ -17,31 +18,12 @@ $.Draw = {
     },
 
     obstacle: function (x, y, size) {
-        var matrix = [
-            [0,1,1,1,1,1,0],
-            [1,3,3,3,2,2,1],
-            [1,2,0,0,0,2,1],
-            [1,2,0,0,0,2,1],
-            [1,2,0,0,0,2,1],
-            [1,2,2,2,2,3,1],
-            [0,1,1,1,1,1,0]
-        ];
-
-        var basex = x;
-
-        // Paint the obstacles as pixels
-        var pixelSize = size / 7;
-        var rock_colors = ["#644e39", "#bc977d", "#997962", "#876b55"];
-
-        for (var i = 0; i < matrix.length; i++) {
-            for (var j = 0; j < matrix[0].length; j++) {
-                if (typeof rock_colors[matrix[i][j]] != 'undefined') {
-                    this.rect(x, y, pixelSize, pixelSize, rock_colors[matrix[i][j]]);
-                }
-                x+= 5;
-            }
-            y += 5;
-            x = basex;
-        }
+        var obstacle_img = document.getElementById('obstacle');
+        var pat = $.ctx.drawImage(obstacle_img, x, y);
     },
+
+    formation: function (x, y, type) {
+        var formation_img = document.getElementById(type);
+        $.ctx.drawImage(formation_img, x, y);
+    }
 };
--- src/js/formation.js
+++ src/js/formation.js
@@ -3,56 +3,16 @@ $.Formation = function (x, y, formationTypeNum) {
 	this.x = x;
 	this.y = y;
     this.vx = $.speed;
-    this.formationType = (formationTypeNum <= 0.5) ? "small" : "large";
+    this.formationType = (formationTypeNum <= 0.5) ? 'small_formation' : 'large_formation';
 
     this.remove = false;
     this.opacity = 1;
     this.fade = 0.01;
     this.pixelSize = 5;
-
-    this.shapes = {
-        large: {
-            palette: ["#80644e", "#876b55", "#644e39"],
-            matrix: [
-                [0,0,0,0,1],
-                [0,0,0,0,1],
-                [0,0,0,0,1],
-                [0,0,0,1,1],
-                [2,0,1,1,2],
-                [2,1,1,1,2],
-                [2,2,1,2,2],
-                [2,2,1,2,2]
-            ]
-        },
-        small: {
-            palette: ["#aa856a", "#bc977d", "#644e39"],
-            matrix: [
-                [0,0,0,1,1],
-                [2,0,1,1,2],
-                [2,2,0,2,2],
-                [2,2,0,2,2]
-            ]
-        }
-    }; 
 };
 
 $.Formation.prototype.render = function () {
-        var basex = this.x,
-            x = this.x,
-            y = this.y,
-            matrix = this.shapes[this.formationType].matrix;
-
-        for (var i = 0; i < matrix.length; i++) {
-            for (var j = 0; j < matrix[0].length; j++) {
-                var color = this.shapes[this.formationType].palette[matrix[i][j]];
-                if (typeof color != 'undefined') {
-                    $.Draw.rect(x, y, this.pixelSize, this.pixelSize, color);
-                }
-                x+= 5;
-            }
-            y += 5;
-            x = basex;
-        }
+    $.Draw.formation(this.x, this.y, this.formationType);
 };
 
 $.Formation.prototype.update = function () {
